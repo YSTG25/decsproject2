723b893619af42b9c517b867e86128720b9580f0
YSTG25 <yashsamant999@gmail.com>
2025-11-07 22:02:16 +0530

    c2

---
+++ build/CMakeFiles/kv_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kv_server.dir/server/kv_server.cpp.o"
+  "CMakeFiles/kv_server.dir/server/kv_server.cpp.o.d"
+  "kv_server"
+  "kv_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kv_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/loadgen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for loadgen.
+# This may be replaced when dependencies are built.
